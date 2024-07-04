bfe7aa16e35d245e0330d62d180ad41a489fec93
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2024-07-04 13:48:04 -0400

    FIX: Remove commented code from .ts files

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,11 +1,4 @@
-// export type DateOptions = {
-//   month: string;
-//   day: string;
-// };
-
 export const DAYS = ['U', 'M', 'T', 'W', 'H', 'F', 'S'];
-
-// export const DATE_OPTIONS: DateOptions = { month: '2-digit', day: '2-digit' };
 
 export const DATE_OPTIONS: Intl.DateTimeFormatOptions = {
   month: '2-digit',
