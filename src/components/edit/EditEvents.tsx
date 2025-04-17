fc02aac8a209ea586e9b6554480ffbaef5c8b62e
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-04-17 01:10:19 -0400

    FIX: Can Add Multiple Blank Topics and Events

--- src/components/edit/EditEvents.tsx
+++ src/components/edit/EditEvents.tsx
@@ -3,24 +3,24 @@ import { Event, Events } from '../../types';
 import EditEvent from './EditEvent';
 
 type EditEventsProps = {
-	topicIndex: number;
-	events: Events;
+  topicIndex: number;
+  events: Events;
 };
 
 function EditEvents({ topicIndex, events }: EditEventsProps) {
-	return (
-		<ul className='EditEvents flex flex-col gap-1'>
-			{events.map((event: Event, i: number) => (
-				<EditEvent
-					where={event.where}
-					when={event.when}
-					topicIndex={topicIndex}
-					eventIndex={i}
-					key={`${topicIndex}: ${event.when}, ${event.where}`}
-				/>
-			))}
-		</ul>
-	);
+  return (
+    <ul className="EditEvents flex flex-col gap-1">
+      {events.map((event: Event, i: number) => (
+        <EditEvent
+          where={event.where}
+          when={event.when}
+          topicIndex={topicIndex}
+          eventIndex={i}
+          key={`${topicIndex}-${i}`}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default EditEvents;
--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -6,26 +6,26 @@ import type { Item } from '../../types';
 import DisplayTopic from './DisplayTopic';
 
 function Preview() {
-	const { data } = useData();
-	const { fontSize, paddingTop } = useFormat();
+  const { data } = useData();
+  const { fontSize, paddingTop } = useFormat();
 
-	return (
-		<div className='Preview relative mx-auto flex flex-col gap-4 border border-black'>
-			<div
-				className={`flex flex-col text-${fontSize} h-full w-full p-4 text-center pt-${paddingTop} gap-4 overflow-hidden`}
-				tabIndex={0}
-				id='preview'
-			>
-				{data.map((item: Item) => (
-					<DisplayTopic
-						topic={item.topic}
-						events={item.events}
-						key={item.topic}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="Preview relative mx-auto flex flex-col gap-4 border border-black">
+      <div
+        className={`flex flex-col text-${fontSize} h-full w-full p-4 text-center pt-${paddingTop} gap-4 overflow-hidden`}
+        tabIndex={0}
+        id="preview"
+      >
+        {data.map((item: Item, i: number) => (
+          <DisplayTopic
+            topic={item.topic}
+            events={item.events}
+            key={`preview-topic-${i}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Preview;
